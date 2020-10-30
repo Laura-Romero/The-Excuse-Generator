69b0f04c0f8f59255f9cc528a4f7acaf6683264c
Laura-Romero <laura90romero@gmail.com>
2020-10-30 12:22:04 +0000

    do it

---
+++ index.js
@@ -0,0 +1,18 @@
+function random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
+window.onload = function() {
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+    let indexWho = random(0,who.length-1); // guarda posicion aleatoria
+    let indexAction = random(0,action.length-1);
+    let indexWhat = random(0,what.lenght-1);
+    let indexWhen = random(0,when.length-1);
+
+    let escusa = who[indexWho]+" "+action[indexAction]+" "+what[indexWhat]+" "+when[indexWhen];
+    document.querySelector("#excuse").innerHTML = escusa;
+}
