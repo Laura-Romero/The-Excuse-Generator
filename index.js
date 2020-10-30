fa6e0c53f7b88de30057674671d939d2296e039f
Laura-Romero <laura90romero@gmail.com>
2020-10-30 12:37:01 +0000

    modified

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ window.onload = function() {
 
     let indexWho = random(0,who.length-1); // guarda posicion aleatoria
     let indexAction = random(0,action.length-1);
-    let indexWhat = random(0,what.lenght-1);
+    let indexWhat = random(0,what.length-1);
     let indexWhen = random(0,when.length-1);
 
     let escusa = who[indexWho]+" "+action[indexAction]+" "+what[indexWhat]+" "+when[indexWhen];
